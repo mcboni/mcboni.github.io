ec4349cfaf331d8b90c8691fdc5a4ec8bdd38a49
mcboni <47659845+mcboni@users.noreply.github.com>
2020-04-28 18:31:50 +0100

    Add files via upload

---
+++ Landing.php
@@ -0,0 +1,408 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/landing.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Invest With Insights</title>
+</head>
+
+<body>
+
+	<header>
+
+	<nav class="navbar navbar-expand-md">
+  <a id="fff2" class="navbar-brand" href="#">Invest With Insights</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <div class="dropdown">
+    <button class="dropbtn">Peronal Finance
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Savings</a>
+      <a href="#">Investment</a>
+      <a href="#">Budgeting</a>
+    </div>
+  </div>
+
+  <div class="dropdown">
+    <button class="dropbtn">Peronal Development
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Life Coach</a>
+      <a href="#">Book Review</a>
+      <a href="#">Read a Book</a>
+    </div>
+  </div>
+      <li class="nav-item">
+        <a id="fff" class="nav-link" href="#">Media</a>
+      </li>
+      <li class="nav-item">
+        <a  id="fff"class="nav-link disabled" href="#">About blog</a>
+      </li>
+    
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    </header>
+
+
+
+    <section>
+    <div class="container">
+        <div class="row latest_post">
+
+           
+            <div class="col-md-8 col-lg-8 col-sm-12 main_content">
+            <h1 class="lp">Latest Posts</h1>
+            <a href="#" id="link">
+            <img src="css/ms.JPG" class="thumbnail" id="main_image">
+            <p class="bottom-left">Personal Investment</p>
+            <h5 id="bottom_note">Things to take note of to excel</h5>
+            <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+
+            </div>
+            </a>
+            
+            <div class="col-md-4 col-lg-4 col-sm-12 section2">
+
+            <h1 class="pp">Popular Posts</h1>
+                <div class="row ">
+
+
+                <a href="#">
+                <div class="first_p">
+                <div class="col">
+                <h3 id="first_heading">What is God telling Us today from His Word can you hear?</h3> <img src="css/iz.jpg" id="image1">
+                  </div>
+                 
+                  <div class="col t_bottom">
+                    <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+                    </div>
+                    </div>
+                </a>
+              
+
+
+                <a href="#">
+                <div class="first_p">
+                <div class="col">
+                <h3 id="first_heading">Tips to reading and assimilating for maximum learning</h3> <img src="css/iz.jpg" id="image1">
+                  </div>
+                 
+                  <div class="col t_bottom">
+                    <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+                    </div>
+                    </div>
+                </a>
+
+
+
+                <a href="#">
+                <div class="first_p">
+                <div class="col">
+                <h3 id="first_heading">Investing the right way for maximum yield that pays well</h3> <img src="css/iz.jpg" id="image1">
+                  </div>
+                 
+                  <div class="col t_bottom">
+                    <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+                    </div>
+                    </div>
+                </a>
+
+
+
+                  <a href="#">
+                <div class="first_p">
+                <div class="col">
+                <h3 id="first_heading">What God is telling us that we need to pay keen attention to but we are not</h3> <img src="css/iz.jpg" id="image1">
+                  </div>
+                 
+                  <div class="col t_bottom">
+                    <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+                    </div>
+                    </div>
+                </a>
+              
+               
+    
+
+                </div> 
+
+            </div>
+            <a href="#" id="link">
+            <div class="col-md-4 col-lg-4 col-sm-12 mini_content">
+
+            <div class="card" style="width: 23rem; border:none;">
+            <img class="card-img-top" src="css/i1.jpg" id="card-image" alt="Card image cap">
+            <p class="bottom-left2">Seeking Proper Direction</p>
+            <div class="card-body">
+            
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Read More...</a>
+            
+            </div>
+            <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+            </div>
+
+            </div>
+            </a>
+
+            <a href="#" id="link">
+            <div class="col-md-4 col-lg-4 col-sm-12 mini_content">
+
+
+            <div class="card" style="width: 23rem; border:none;">
+            <img class="card-img-top" src="css/i2.jpg" id="card-image"  alt="Card image cap">
+            <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+            </div>
+
+
+
+            </div>
+            </a>
+
+            <a href="#" id="link">
+            <div class="col-md-4 col-lg-4 col-sm-12 mini_content">
+
+
+            <div class="card" style="width: 23rem; border:none;">
+            <img class="card-img-top" src="css/i3.jpg"  id="card-image" alt="Card image cap">
+            <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Read More...</a>
+            </div>
+            <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+            </div>
+
+
+
+            </div>
+            </a>
+
+
+        </div>
+
+
+
+
+    </div>
+    </section>
+
+    <section>
+        <div class="container-fluid">
+            <div class="row subscribe">
+            <div class="col-lg-6 col-md-6 col-sm-12 text-center form_section">
+            <h2 id="sub1">Never Miss a Story</h2>
+            <p id="sub2"> Subscribe to our News Letter to remian informed about the real issues of life</p>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 text-center">
+              <form  action="" method="post">
+              <input type="email" name="email" class="form-control2" placeholder="Email Address" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+              <input type="submit" name="txt" value="subscribe" id='submit'>
+            
+			
+		
+				
+			</form>
+				
+			</div>
+
+            </div>
+
+
+        </div>
+
+
+
+
+    </section>
+
+    <section>
+    <div class="container">
+        <div class="row">
+
+        <a href="#" id="link">
+        <div class="col-md-4 col-lg-4 col-sm-12 mini_content">
+
+
+            <div class="card" style="width: 23rem; border:none;">
+            <img class="card-img-top" src="css/i4.jpg" id="card-image"  alt="Card image cap">
+            <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Read More...</a>
+            </div>
+            <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+            </div>
+
+
+
+            </div>
+            </a>
+
+
+
+            <a href="#" id="link">
+            <div class="col-md-4 col-lg-4 col-sm-12 mini_content">
+
+
+                <div class="card" style="width: 23rem; border:none;">
+                <img class="card-img-top" src="css/i5.jpg" id="card-image"  alt="Card image cap">
+                <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Read More...</a>
+                </div>
+                <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+                </div>
+
+
+
+            </div>
+
+
+            </a>
+
+
+            
+            <a href="#" id="link">
+            <div class="col-md-4 col-lg-4 col-sm-12 mini_content">
+
+
+                <div class="card" style="width: 23rem; border:none;">
+                <img class="card-img-top" src="css/i6.jpg" id="card-image" alt="Card image cap">
+                <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Read More...</a>
+                </div>
+                <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+                </div>
+
+
+
+            </div>
+            </a>
+
+
+
+
+            
+            <a href="#" id="link">
+            <div class="col-md-4 col-lg-4 col-sm-12 mini_content2">
+
+
+                <div class="card" style="width: 23rem; border:none;">
+                <img class="card-img-top" src="css/i7.jpg" id="card-image" alt="Card image cap">
+                <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Read More...</a>
+                </div>
+                <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+                </div>
+
+
+
+            </div>
+            </a>
+
+
+
+            
+            <a href="#" id="link">
+            <div class="col-md-4 col-lg-4 col-sm-12 mini_content2">
+
+
+                <div class="card" style="width: 23rem; border:none;">
+                <img class="card-img-top" src="css/i8.jpg" id="card-image"  alt="Card image cap">
+                <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Read More...</a>
+                </div>
+                <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+                </div>
+
+
+
+            </div>
+            </a>
+
+
+
+            
+            <a href="#" id="link">
+            <div class="col-md-4 col-lg-4 col-sm-12 mini_content2">
+
+
+                <div class="card" style="width: 23rem; border:none;">
+                <img class="card-img-top" src="css/i9.jpg" id="card-image"  alt="Card image cap">
+                <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Read More...</a>
+                </div>
+                <span id="time1"><date>10th May 2020</date> 8mins Read</span>
+                </div>
+
+
+
+            </div>
+            </a>
+
+
+        
+     </div>
+    </div>
+</section>
+
+
+<section>
+<div class="container-fluid">
+    <div class="row footer">
+        <div class="col-lg-12 col-md-12 col-sm-12  text-center foot1">
+<h1>Content of the footer will be determined later by you. This space is just to show you the layout</h1>
+
+        </div>
+
+
+   
+
+
+
+
+
+
+
+
+
+
+</div>
+</div>
+</section>
+    
+
+
+</body>
+<!-- jQuery first-->
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+<!--- Script for bootstrap-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
